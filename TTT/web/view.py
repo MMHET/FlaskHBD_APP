02fd4d6f23038543d6980e999fb7360f660f41bd
MMHET <snapo2579@gmail.com>
2024-09-26 02:17:17 +0200

    Update view.py

--- TTT/web/view.py
+++ TTT/web/view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,render_template
 import time
 
-view = Blueprint('view',__name__,template_folder='web/static')
+view = Blueprint('view',__name__,template_folder='TTT\web\static')
 
 def timelocal():
     curr_time = time.strftime("%H:%M", time.localtime())
